e650eca089fa9df2a3da2b174a6160d9344ecd81
Gustavo Segura <yo@gustavosegura.com>
2018-10-15 19:20:10 -0600

    [fix]	Comma error in materials view component

--- src/app/components/view-materials/view-materials.component.ts
+++ src/app/components/view-materials/view-materials.component.ts
@@ -13,31 +13,32 @@ export class ViewMaterialsComponent implements OnInit {
   edit_material: boolean;
   material_list: Material[] = [];
   material_to_add: Material;
-  header_list: string[] = []
+  material_to_edit: Material;
+  header_list: string[] = [];
   constructor() {
     this.add_material = false;
     this.edit_material = false;
-    this.material_to_add = new Material(1, "", "", 0)
-    this.material_to_edit = new Material(1, "", "", 0)
+    this.material_to_add = new Material(1, "", "", 0);
+    this.material_to_edit = new Material(1, "", "", 0);
     this.header_list = ["Nombre", "Descripcion", "Precio", "", ""];
-    this.material_list.push(new Material(0, "varilla", 'Consequatur est quas id vero aut perspiciatis quod dolor. Libero non ut modi aut reiciendis dolorem expedita. Tenetur sed consequuntur quis debitis eos rerum vel. Dolorem consequatur ut qui laboriosam voluptatibus assumenda reprehenderit dolore. Est quae voluptatem deleniti quod voluptas. Sunt harum ullam dolore minus est dicta deserunt.', 1000))
+    this.material_list.push(new Material(0, "varilla", 'Consequatur est quas id vero aut perspiciatis quod dolor. Libero non ut modi aut reiciendis dolorem expedita. Tenetur sed consequuntur quis debitis eos rerum vel. Dolorem consequatur ut qui laboriosam voluptatibus assumenda reprehenderit dolore. Est quae voluptatem deleniti quod voluptas. Sunt harum ullam dolore minus est dicta deserunt.', 1000));
     this.material_list.push(new Material(1, "perlin", 'perlin de hierro de 3m', 1000));
     this.material_list.push(new Material(2, "block", 'brock de 30*10cm', 1000));
-    this.material_list.push(new Material(3, "cemento", 'saco de 50kg4', 1000));;
+    this.material_list.push(new Material(3, "cemento", 'saco de 50kg4', 1000));
     console.log(this.material_list);
   }
 
   ngOnInit() {
   }
   onSubmitAdd() {
-    this.material_to_add.id=this.material_list.length
+    this.material_to_add.id = this.material_list.length;
     this.material_list.push(this.material_to_add);
     this.add_material = false;
   }
   onSubmitEdit() {
     for (let material of this.material_list) {
       if (material.id === this.material_to_edit.id) {
-        material = this.material_to_edit
+        material = this.material_to_edit;
       }
     }
     this.edit_material = false;
@@ -58,16 +59,14 @@ export class ViewMaterialsComponent implements OnInit {
     }
   }
   delete_material(id) {
-    console.log("delete", id)
+    console.log("delete", id);
 
     for (let i = 0; i < this.material_list.length; i++) {
-      if (i, this.material_list[i].id == id) {
-        console.log(this.material_list[i])
+      if (this.material_list[i].id == id) {
+        console.log(this.material_list[i]);
         this.material_list.splice(i, 1);
         break;
       }
     }
-
   }
-
 }
