03616a5d9f02cdd5ae35e475a238d6b3eb977fa5
Gustavo Segura <yo@gustavosegura.com>
2018-10-25 22:40:34 -0600

    [add]	use component parameters

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  // current_page:string= "aasas";
+  @Input() current_page:string;
+  constructor() {
+    console.log("current page",this.current_page);
+  }
 
   ngOnInit() {
   }
